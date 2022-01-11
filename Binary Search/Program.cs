4eff2b9f1846bf9dccc726f3c74be683603053dd
Finley Edwards <EDW17006550@btc.ac.uk>
2022-01-11 14:19:37 +0000

    Added comments.

--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -1,8 +1,8 @@
-﻿List<int> A = new List<int>() { 3, 5, 2, 1, 7, 4, 19, 4, 5, 9, 10, 11 };
-int X = 19;
+﻿List<int> A = new List<int>() { 3, 5, 2, 1, 7, 4, 19, 4, 5, 9, 10, 11 }; //The list of items going to be searched through.
+int X = 19; //The integer being searched for.
 
-A.Sort();
+A.Sort(); //This sorts the list, so the binary search will work with it.
 
 
-int itemFound = A.BinarySearch(X);
+int itemFound = A.BinarySearch(X); //This will perform a binary search for x using a predefined function.
 Console.WriteLine($"The number {X} was found in position {itemFound}.");
