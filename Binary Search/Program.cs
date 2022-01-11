f438b7226128174cd4116c973d9c63b7b5c0ec3f
Finley Edwards <EDW17006550@btc.ac.uk>
2022-01-11 14:16:21 +0000

    Finished the sorting program

--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿List<int> A = new List<int>() { 3, 5, 2, 1, 7, 4, 19, 4, 5, 9, 10, 11 };
+int X = 19;
+
+A.Sort();
+
+
+int itemFound = A.BinarySearch(X);
+Console.WriteLine($"The number {X} was found in position {itemFound}.");
